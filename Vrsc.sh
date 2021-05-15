deb954f9fdd0a2b87eb0c12e6eca5a20b8f62313
fackiechamuch <45721866+fackiechamuch@users.noreply.github.com>
2021-05-15 21:59:25 +0800

    Create Vrsc.sh

---
+++ Vrsc.sh
@@ -0,0 +1,5 @@
+#vrsc
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-linux-v0.8.2.tgz
+tar -zxvf nheqminer-linux-v0.8.2.tgz 
+tar -zxvf nheqminer-Linux-v0.8.2.tar.gz 
+nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RXMmo9iCr6nwgLwuCtU5a2WYEDvexXrphw.uwa2 -p x -t 4
